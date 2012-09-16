2a8996220a0c84346071f4ce2120ba7ef226df1f
Michael L Perry <michael@qedcode.com>
2012-09-15 21:27:38 -0500

    Added Sales bounded context.

---
+++ Sales/PharmaNet.Sales.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PharmaNet.Sales.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
