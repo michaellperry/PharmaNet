4b3aff73b1ca6a40ecbd58535ae5477b315709f4
Michael L Perry <michael@qedcode.com>
2012-09-22 21:43:50 -0500

    Added factory method to performance calculator.

--- Performance/PharmaNet.Performance.Application/PerformanceCalculator.cs
+++ Performance/PharmaNet.Performance.Application/PerformanceCalculator.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PharmaNet.Performance.Domain;
 
 namespace PharmaNet.Performance.Application
 {
     public class PerformanceCalculator
     {
+        public ReportCard CalculatePerformance(
+            Member member,
+            MeasurementPeriod measurementPeriod,
+            Rebate rebate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
